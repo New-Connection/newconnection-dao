2635b86fb285e8fba1f6318c0d2477c6bf638ed1
hokage229 <ernestzulpukarov@gmail.com>
2022-06-13 16:40:25 +0600

    refactor GovernanceNFT

---
+++ contracts/GovernanceNFT.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GovernanceNFT is ERC721, ERC721Enumerable, EIP712, ERC721Votes, Ownable {
+    uint256 public constant MAX_SUPPLY = 10000;
+    uint256 public constant MAX_PUBLIC_MINT = 5;
+    uint256 public constant PRICE_PER_TOKEN = 0.1 ether;
+
+    bool public saleIsActive = false;
+    string private _baseURIextended;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) EIP712(name, "1") {}
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function mint(uint256 numberOfTokens) public payable {
+        uint256 supply = totalSupply();
+        require(saleIsActive, "Sale must be active to mint tokens");
+        require(numberOfTokens <= MAX_PUBLIC_MINT, "Exceeded max token purchase");
+        require(supply + numberOfTokens <= MAX_SUPPLY, "Purchase would exceed max tokens");
+        require(PRICE_PER_TOKEN * numberOfTokens <= msg.value, "Ether value sent is not correct");
+
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    function reserve(uint256 n) public onlyOwner {
+        uint256 supply = totalSupply();
+        uint256 i;
+        for (i = 0; i < n; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    function setSaleState(bool newState) public onlyOwner {
+        saleIsActive = newState;
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseURIextended = baseURI;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Votes) {
+        super._afterTokenTransfer(from, to, tokenId);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+    }
+}
--- deploy/03-deploy-nft-example.ts
+++ deploy/03-deploy-nft-example.ts
@@ -1,14 +1,15 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { NFT_NAME, NFT_SYMBOL } from "../helper-hardhat-config";
 
 const deployNFTExample: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { getNamedAccounts, deployments } = hre;
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    await deploy("NFTExample", {
+    await deploy("GovernanceNFT", {
         from: deployer,
-        args: [],
+        args: [NFT_NAME, NFT_SYMBOL],
         log: true,
     });
 };
